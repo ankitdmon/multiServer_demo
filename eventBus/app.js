bd802a1efc9e033f5564ec0c2526eb25282adb39
Ankit Mishra <ankitmishra2108@gmail.com>
2023-09-01 11:09:04 +0530

    Added all the files

--- eventBus/app.js
+++ eventBus/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const exios = require('axios');
+const axios = require('axios');
 require('dotenv').config();
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(bodyParser.json());
 
+app.post("/events", (req, res) => {
+    const event = req.body;
+    axios.post('http://localhost:9090/events');  // For post service
+    axios.post('http://localhost:8080/events');  // For comment service
+    // axios.post('http://localhost:1010/events');  // for query service
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on PORT: ${PORT}`);
