5ff334a6134c576f44c684fe92bdf66cadc14a39
Ankit Mishra <ankitmishra2108@gmail.com>
2023-09-01 11:56:15 +0530

    Recieved event in comment service

--- server/comments/app.js
+++ server/comments/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv");
@@ -11,6 +11,13 @@ const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/", router);
+
+// event bus reciever
+
+app.post("/events", (req, res) => {
+  console.log("Event Recieved!", req, body.type);
+  req.setEncoding({});
+});
 
 app.listen(PORT, () => {
   console.log(`Comment server is running on PORT:${PORT}`);
